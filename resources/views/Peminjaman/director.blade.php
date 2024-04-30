e33571b096e7e8d09c7cfdb3ac2e161c7ee5fd31
squadNonton <nontonsquad@gmail.com>
2024-04-30 13:02:00 +0700

    rev-view-role

--- resources/views/Peminjaman/director.blade.php
+++ resources/views/Peminjaman/director.blade.php
@@ -67,10 +67,9 @@
                                                 <td>{{ $value->ast_name }} - {{ $value->ast_no }}</td>
                                                 <td>{{ $value->necessity }}</td>
                                                 <td>
-                                                    <button type="button" class="btn btn-danger btn-sm" data-name="canceled" data-item="{{ $value->id }}">
-                                                        Cancel
-                                                    </button>
-                                                    <button type="button" class="btn btn-danger btn-sm" data-name="approve" data-item="{{ $value->id }}">
+                                                    {{-- x --}}
+                                                    <button type="button" class="btn btn-danger btn-sm" data-name="approve"
+                                                        data-item="{{ $value->id }}">
                                                         Returned
                                                     </button>
                                                 </td>
@@ -117,9 +116,9 @@
         $(document).on("click", "[data-name='approve']", function(e) {
             var id = $(this).attr("data-item");
             var id_director = "{!! $idn_user->id !!}";
-            var date            = new Date();
-            var datetime        = moment(date);
-            var director_detail   = '["'+datetime.format('YYYY-MM-DD HH:mm:ss')+'","-"]';
+            var date = new Date();
+            var datetime = moment(date);
+            var director_detail = '["' + datetime.format('YYYY-MM-DD HH:mm:ss') + '","-"]';
             var status = 5;
 
             var table = "trx_assets_landing";
@@ -190,12 +189,12 @@
         });
 
         $(document).on("click", "[data-name='save_noted']", function(e) {
-            var id              = $("[data-name='id_trx_assets_landing']").val();
-            var note            = $("[data-name='text_note']").val();
-            var id_director     = "{!! $idn_user->id !!}";
-            var date            = new Date();
-            var datetime        = moment(date);
-            var director_detail = '["'+datetime.format('YYYY-MM-DD HH:mm:ss')+'","'+note+'"]';
+            var id = $("[data-name='id_trx_assets_landing']").val();
+            var note = $("[data-name='text_note']").val();
+            var id_director = "{!! $idn_user->id !!}";
+            var date = new Date();
+            var datetime = moment(date);
+            var director_detail = '["' + datetime.format('YYYY-MM-DD HH:mm:ss') + '","' + note + '"]';
             var status = 6;
 
             var table = "trx_assets_landing";
--- resources/views/Peminjaman/navtab.blade.php
+++ resources/views/Peminjaman/navtab.blade.php
@@ -1,98 +1,166 @@
 <ul class="nav nav-tabs">
 
     @php
-        $role_data    = ['1','2','3','4','5','6','7','8','9','10','11'];
+        $role_data = [
+            '1',
+            '7',
+            '9',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15',
+            '16',
+            '17',
+            '18',
+            '19',
+            '20',
+            '21',
+            '22',
+            '23',
+            '24',
+            '25',
+        ];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetsdash'];
+            $array_route = ['assetsdash'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetsdash') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Dashboard</a>
+            <a href="{{ route('assetsdash') }}"
+                class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Dashboard</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','2','3','4','5','6','7','9','10','11'];
+        $role_data = [
+            '1',
+            '7',
+            '9',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15',
+            '16',
+            '17',
+            '18',
+            '19',
+            '20',
+            '21',
+            '22',
+            '23',
+            '24',
+            '25',
+        ];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetscreate'];
+            $array_route = ['assetscreate'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetscreate') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Create Form</a>
+            <a href="{{ route('assetscreate') }}" class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Create
+                Form</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','7','10'];
+        $role_data = ['1', '7', '8', '9', '10'];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetsdephed'];
+            $array_route = ['assetsdephed'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetsdephed') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">DepHead Approved</a>
+            <a href="{{ route('assetsdephed') }}"
+                class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">DepHead Approved</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','2','11'];
+        $role_data = ['1', '16'];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetsfirst'];
+            $array_route = ['assetsfirst'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetsfirst') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">HRGA Approved</a>
+            <a href="{{ route('assetsfirst') }}" class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">HRGA
+                Approved</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','8'];
+        $role_data = ['1', '24'];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetssecond'];
+            $array_route = ['assetssecond'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetssecond') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Security</a>
+            <a href="{{ route('assetssecond') }}"
+                class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Security</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','8'];
+        $role_data = ['1', '24'];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetsdirector'];
+            $array_route = ['assetsdirector'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetsdirector') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Returned</a>
+            <a href="{{ route('assetsdirector') }}"
+                class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Returned</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','2','3','4','5','6','7','8','9','10','11'];
+        $role_data = [
+            '1',
+            '7',
+            '9',
+            '10',
+            '11',
+            '12',
+            '13',
+            '14',
+            '15',
+            '16',
+            '17',
+            '18',
+            '19',
+            '20',
+            '21',
+            '22',
+            '23',
+            '24',
+            '25',
+        ];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['assetsdata'];
+            $array_route = ['assetsdata'];
         @endphp
         <li class="nav-item" role="presentation">
-            <a href="{{ route('assetsdata') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Show Lending Asset</a>
+            <a href="{{ route('assetsdata') }}" class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Show
+                Lending Asset</a>
         </li>
     @endif
 
     @php
-        $role_data    = ['1','2','9','10','11'];
+        $role_data = ['1', '8', '16', '22'];
     @endphp
-    @if(in_array($idn_user->role_id , $role_data))
+    @if (in_array($idn_user->role_id, $role_data))
         @php
-            $array_route    = ['dataasset'];
+            $array_route = ['dataasset'];
         @endphp
         <li class="nav-item">
-            <a href="{{ route('dataasset') }}" class="nav-link @if (in_array(Route::currentRouteName() , $array_route)) active @endif">Data Asset</a>
+            <a href="{{ route('dataasset') }}" class="nav-link @if (in_array(Route::currentRouteName(), $array_route)) active @endif">Data
+                Asset</a>
         </li>
     @endif
 </ul>
